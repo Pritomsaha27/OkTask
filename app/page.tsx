f822d071b31c3ddfbb73180f4a6bb827202ad8ed
pritom saha <pritom2001.saha@gmail.com>
2025-01-22 21:35:45 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
 
 const page = () => {
   return (
-    <div>page</div>
+    <>
+    <div className="container m-auto">
+    <div className=" justify-around items-center mt-24 px-24 flex">
+      <div >
+      <p className="text-6xl font-bold text-center">Organize your <br /> work and <br /> life, finally. </p>
+      <p className="text-xl mt-6 text-center text-slate-600">Simplify Your Life With World's <br /> Best ToDo App</p>
+      <button className="text-center button-53 mt-6">Start for Free</button>
+      </div>
+      <img className="w-2/4" src="/landingImg.jpg" alt="" />
+    </div>
+      <img src="/wave.jpg" alt="" />
+    </div>
+    </>
   )
 }
 
